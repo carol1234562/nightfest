d0b8ee486b29c0dcb3b41d38a5549dda78fdd1fd
Carolina <csolorzanoomar1419@gmail.com>
2026-04-07 10:44:08 +0200

    correcion base de datos

--- src/Controller/UserControler.php
+++ src/Controller/UserControler.php
@@ -14,7 +14,7 @@ class UserController {
         $host = "localhost";
         $user = "root";      // Cambiar si usas otro usuario
         $pass = "";          // Cambiar si tienes contraseña en MySQL
-        $db   = "NightFest BD";
+        $db   = "NightFest";
 
         $this->connection = new mysqli($host, $user, $pass, $db);
 
